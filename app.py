b6640b513f30723812f779b2e67e888db142e378
Peter Yin <peteryin@transcriptic.com>
2016-06-11 02:06:53 -0700

    putting meat on dem bones

--- app.py
+++ app.py
@@ -1,15 +1,66 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+from twilio.rest import TwilioRestClient
+from datetime import datetime as dt
+import json
+
+
+ACCOUNT_SID = os.environ["TWILIO_ACCOUNT_SID"]
+AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
+
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
+global start_time
+global sesh_id
 
-    resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
-    return str(resp)
+start_time = None
+
+def analyze_messages(messages):
+	hot = 0
+	cold = 0
+	for msg in messages:
+		if msg.direction == "inbound":
+			if msg.date_created > start_time:
+				if "hot" in msg.body:
+					hot += 1
+				elif "not" in msg.body:
+					cold += 1
+	result = {"hot": hot, "cold": cold}
+
+
+@app.route("/submit/", methods=['GET', 'POST'])
+def start_poll(sesh_id=None):
+	resp = twilio.twiml.Response()
+	resp.message("Thanks for your input! ")
+	return str(resp)
+
+
+@app.route("/start_poll/<id>", methods=['GET', 'POST'])
+def start_poll(sesh_id=None):
+	sesh_id = sesh_id
+    start_time = dt.utcnow()
+
+
+@app.route("/get_results/", methods=['GET', 'POST'])
+def get_results()
+	# get all messages from today
+	messages = client.messages.list(date_sent=dt.utcnow())
+	result = analyze_messages(messages)
+	return json.dumps(result)
+
+
+@app.route("/stop_poll/", methods=['GET', 'POST'])
+def stop_poll()
+	# Delete all messages from today
+	messages = client.messages.list(date_sent=dt.utcnow())
+	for msg in messages:
+		client.messages.delete(msg.sid)
+
+	# Reset start time and sesh_id
+	start_time = None
+	sesh_id = None
 
 if __name__ == "__main__":
     app.run(debug=True)
